cd0fa3ea224ef58a717233776688a3d3b51b0948
SeungWookHan <hswook12@me.com>
2021-10-03 16:32:31 +0000

    Initialize images server

---
+++ index.js
@@ -0,0 +1,13 @@
+// Libraries
+const express = require("express");
+
+const PORT = 80;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
